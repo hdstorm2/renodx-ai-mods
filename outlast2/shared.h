d7908aa82aa553f2663b04a54c030b7f979e435d
hdstorm2 <hdxxstormersxxxyt2@gmail.com>
2026-08-24 22:55:30 +0200

    Fix shared.h with proper RenoDX integration

--- outlast2/shared.h
+++ outlast2/shared.h
@@ -1,10 +1,79 @@
-#pragma once
+#ifndef SRC_GAMES_OUTLAST2_SHARED_H_
+#define SRC_GAMES_OUTLAST2_SHARED_H_
 
+// HDR Configuration Presets
+// #define RENODX_PEAK_WHITE_NITS                 1000.f
+// #define RENODX_DIFFUSE_WHITE_NITS              203.f
+// #define RENODX_GRAPHICS_WHITE_NITS             203.f
+// #define RENODX_COLOR_GRADE_STRENGTH            1.f
+// #define RENODX_TONE_MAP_TYPE                   TONE_MAP_TYPE_RENO_DRT
+// #define RENODX_TONE_MAP_EXPOSURE               1.f
+// #define RENODX_TONE_MAP_HIGHLIGHTS             50.f
+// #define RENODX_TONE_MAP_SHADOWS                50.f
+// #define RENODX_TONE_MAP_CONTRAST               50.f
+// #define RENODX_TONE_MAP_SATURATION             50.f
+// #define RENODX_TONE_MAP_HIGHLIGHT_SATURATION   50.f
+// #define RENODX_TONE_MAP_BLOWOUT                0.f
+// #define RENODX_TONE_MAP_FLARE                  0.f
+// #define RENODX_TONE_MAP_HUE_CORRECTION         100.f
+// #define RENODX_TONE_MAP_HUE_SHIFT              50.f
+// #define RENODX_TONE_MAP_WORKING_COLOR_SPACE    color::convert::COLOR_SPACE_BT709
+// #define RENODX_TONE_MAP_CLAMP_COLOR_SPACE      color::convert::COLOR_SPACE_NONE
+// #define RENODX_TONE_MAP_CLAMP_PEAK             color::convert::COLOR_SPACE_BT709
+// #define RENODX_TONE_MAP_HUE_PROCESSOR          HUE_PROCESSOR_OKLAB
+// #define RENODX_GAMMA_CORRECTION                GAMMA_CORRECTION_GAMMA_2_2
+// #define RENODX_SWAP_CHAIN_ENCODING             ENCODING_SCRGB
+// #define RENODX_SWAP_CHAIN_ENCODING_COLOR_SPACE color::convert::COLOR_SPACE_BT709
+
+// Must be 32bit aligned (16 floats = 64 bytes)
 struct ShaderInjectData {
-  // Tone mapping
-  float tone_map_type = 1.f;  // 0 = Vanilla, 1 = RenoDX
-  float peak_white_nits = 1000.f;
-  float diffuse_white_nits = 203.f;
-  float graphics_white_nits = 203.f;
-  float gamma_correction = 1.f;  // 0 = Off, 1 = 2.2 EOTF
+  float peak_white_nits;
+  float diffuse_white_nits;
+  float graphics_white_nits;
+  float scene_grade_strength;
+  float tone_map_type;
+  float tone_map_exposure;
+  float tone_map_highlights;
+  float tone_map_shadows;
+  float tone_map_contrast;
+  float tone_map_saturation;
+  float tone_map_highlight_saturation;
+  float tone_map_blowout;
+  float tone_map_flare;
+  float tone_map_hue_correction;
+  float tone_map_hue_shift;
+  float gamma_correction;
 };
+
+#ifndef __cplusplus
+#if ((__SHADER_TARGET_MAJOR == 5 && __SHADER_TARGET_MINOR >= 1) || __SHADER_TARGET_MAJOR >= 6)
+cbuffer shader_injection : register(b13, space50) {
+#elif (__SHADER_TARGET_MAJOR < 5) || ((__SHADER_TARGET_MAJOR == 5) && (__SHADER_TARGET_MINOR < 1))
+cbuffer shader_injection : register(b13) {
+#endif
+  ShaderInjectData shader_injection : packoffset(c0);
+}
+
+#define RENODX_TONE_MAP_TYPE                 shader_injection.tone_map_type
+#define RENODX_PEAK_WHITE_NITS               shader_injection.peak_white_nits
+#define RENODX_DIFFUSE_WHITE_NITS            shader_injection.diffuse_white_nits
+#define RENODX_GRAPHICS_WHITE_NITS           shader_injection.graphics_white_nits
+#define RENODX_GAMMA_CORRECTION              shader_injection.gamma_correction
+#define RENODX_TONE_MAP_EXPOSURE             shader_injection.tone_map_exposure
+#define RENODX_TONE_MAP_HIGHLIGHTS           shader_injection.tone_map_highlights
+#define RENODX_TONE_MAP_SHADOWS              shader_injection.tone_map_shadows
+#define RENODX_TONE_MAP_CONTRAST             shader_injection.tone_map_contrast
+#define RENODX_TONE_MAP_SATURATION           shader_injection.tone_map_saturation
+#define RENODX_TONE_MAP_HIGHLIGHT_SATURATION shader_injection.tone_map_highlight_saturation
+#define RENODX_TONE_MAP_BLOWOUT              shader_injection.tone_map_blowout
+#define RENODX_TONE_MAP_FLARE                shader_injection.tone_map_flare
+#define RENODX_TONE_MAP_HUE_CORRECTION       shader_injection.tone_map_hue_correction
+#define RENODX_TONE_MAP_HUE_SHIFT            shader_injection.tone_map_hue_shift
+#define RENODX_COLOR_GRADE_STRENGTH          shader_injection.scene_grade_strength
+#define RENODX_RENO_DRT_TONE_MAP_METHOD      renodx::tonemap::renodrt::config::tone_map_method::REINHARD
+
+#include "../../shaders/renodx.hlsl"
+
+#endif
+
+#endif  // SRC_GAMES_OUTLAST2_SHARED_H_
